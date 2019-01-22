387fdda858861a1f75ba3e3812af1dac1291d073
sanfer <sanfer@anonymous.home>
2019-01-21 20:12:16 -0600

    Initial version - Added services and components to UI

---
+++ src/app/components/admin/admin.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { BikeService } from '../../services/bike.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+  public bikes;
+
+  constructor(private bikeService: BikeService) { }
+
+  ngOnInit() {
+  }
+
+  getBikes() {
+    this.bikeService.getBikes().subscribe(
+      data => { this.bikes = data },
+      err => console.error(err),
+      () => console.log('Bikes Loaded')
+    );
+  }
+}
